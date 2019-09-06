d37f4be86bb9c511df0e1b645b9db70b6dd015af
milos2018 <milos.antic.07104@gmail.com>
2019-09-06 14:05:00 +0200

    scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,9 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.angular
+    where subtitile: "Angular"
+  end
+
+  scope :ruby_on_rails_portfolio_item, lambda { where(subtitile: "Ruby on rails") }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+  Topic.create!(
+         title: "Topic #{topic}"
+  )
+end
+p "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -15,7 +22,8 @@
  ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-officia deserunt mollit anim id est laborum."
+officia deserunt mollit anim id est laborum.",
+    topic_id: Topic.last.id
   )
 end
 p "10 blog posts created"
@@ -28,14 +36,23 @@ p "10 blog posts created"
 end
 p "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Porfolio title: #{portfolio_item}",
-    subtitile: "Subtitle: #{portfolio_item}",
+    subtitile: "Ruby on rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
  ut labore et dolore magna aliqua.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/300x200"
   )
 end
-p "9 portfolio items created"
+p "8 portfolio items created"
+
+Portfolio.create!(
+  title: "Porfolio title: eee",
+  subtitile: "Angular",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+ ut labore et dolore magna aliqua.",
+  main_image: "https://via.placeholder.com/600x400",
+  thumb_image: "https://via.placeholder.com/300x200"
+)
