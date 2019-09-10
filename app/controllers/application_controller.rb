a556e5dd4e9f22b9477f508199e57945664d496e
milos2018 <milos.antic.07104@gmail.com>
2019-09-10 20:15:56 +0200

    virtual attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
 
-  before_filter :configure_permitted_parameters if :devise_controller?
+  #before_filter :configure_permitted_parameters if :devise_controller?
 
-  def configure_permitted_parameters
-    devise_perameters_sanitizer.permit(:sign_up, keys: [:name])
-    devise_perameters_sanitizer.permit(:account_update, keys: [:name])
-  end
+  #def configure_permitted_parameters
+  #  devise_perameters_sanitizer.permit(:sign_up, keys: [:name])
+  #  devise_perameters_sanitizer.permit(:account_update, keys: [:name])
+  #end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def user_name
+    self.email.split("@").first
+  end
 end
