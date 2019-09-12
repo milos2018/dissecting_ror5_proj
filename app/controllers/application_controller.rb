6389e9e8ccf5e09a8061043893a67b0cbb815388
milos2018 <milos.antic.07104@gmail.com>
2019-09-12 14:09:15 +0200

    refactor session set source to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
+  include SetSource
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :set_source
-
   protected
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
