0a6bfefad14e21eded23f9d5b2153532313847cd
milos2018 <milos.antic.07104@gmail.com>
2019-09-12 19:02:43 +0200

    current user change with OpenStruct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,5 @@
 class ApplicationController < ActionController::Base
+  include DeviseWhitelist
   include SetSource
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
-
-  #before_filter :configure_permitted_parameters if :devise_controller?
-
-  #def configure_permitted_parameters
-  #  devise_perameters_sanitizer.permit(:sign_up, keys: [:name])
-  #  devise_perameters_sanitizer.permit(:account_update, keys: [:name])
-  #end
+  include CurrentUserConcern
 end
